709076642a34ebc3945b2d884f5747b60e6956ed
nihar-simhadri <techtraffic19@gmail.com>
2020-11-16 12:55:54 +0530

    fahrenheit
    my program for fahrenheit to celsius

---
+++ fahrenheit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+
+   int fahr , celsius ;
+   int lower, upper,increment;
+   lower = 0;
+   upper = 300;
+   increment=5;
+   celsius=lower;
+   while(celsius<=upper){
+    fahr=(celsius*9/5)+32;
+    printf("%d\t%d\n",celsius,fahr);
+    celsius=celsius+increment;
+      }
+
+   }
+   
+
+   
+   
