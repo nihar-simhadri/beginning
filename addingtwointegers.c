cd4024afbb81139b58e68d7875619ba8c6dc640b
nihar-simhadri <techtraffic19@gmail.com>
2021-01-07 20:34:49 +0530

    added two numbers uaing c

---
+++ addingtwointegers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int value1,value2,sum;
+    value1=15;
+    value2=55;
+    sum=value1+value2;
+    printf("the sum of %i and %i is %i",value1,value2,sum);
+    return 0;
+}
