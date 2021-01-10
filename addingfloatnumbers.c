bcd613fbcc2f6ee8e688586f69e050ccf1c2d194
nihar-simhadri <techtraffic19@gmail.com>
2021-01-10 21:13:42 +0530

    adding user input float numbers

---
+++ addingfloatnumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float m , n , sum ;
+    printf("enter the first number ");
+    scanf("%f",&m);
+    printf("enter the second number");
+    scanf("%f",&n);
+    sum=m+n;
+    printf("the sum of %f,%f is %f",m,n,sum);
+    return 0 ;
+
+}
