4ea3f03fa41e60bd29941c55b0e9215152e9b173
nihar-simhadri <techtraffic19@gmail.com>
2021-01-11 16:41:24 +0530

    sum of all add numbers in a given limit

--- sumofalloddnumbers.c
+++ sumofalloddnumbers.c
@@ -1,19 +1,23 @@
-# include <stdio.h> 
-int main() 
-{ 
- int i, n ,sum ;  
- printf("Enter the limit : ") ; 
- scanf("%d", &n) ; 
-   printf("\n Odd Numbers between 0 and %d are : ", n);
-  for(i = 1; i <= n; i++)
+#include<stdio.h>
+ 
+int main()
+{
+  int i, number, Sum = 0;
+ 
+  printf("\n Please Enter the Maximum Limit Value : ");
+  scanf("%d", &number);
+  
+  printf("\n Odd Numbers between 0 and %d are : ", number);
+  for(i = 1; i <= number; i++)
   {
   	if ( i%2 != 0 ) 
   	{
   		printf("%d  ", i);
-        sum = sum + i;
+        Sum = Sum + i;
   	}
   }
-  printf("\n \n The Sum of Odd Numbers from 1 to %d  = %d", n, sum);
+  printf("\n \n The Sum of Odd Numbers from 1 to %d  = %d", number, Sum);
 
   return 0;
 }
+
