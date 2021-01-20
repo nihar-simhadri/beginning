ef7e869385249d2ecbddfac6ea4e4d61a48e14c0
nihar-simhadri <techtraffic19@gmail.com>
2021-01-20 15:08:13 +0530

    ternary operator
    here if a and b are equal it prints equal or else prints not equaL USING TERNARY OPERATOR

---
+++ ternanryoperator.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a , b , equal ,notequal;
+    printf("enter the value of both a and b :");
+    scanf("%d%d",&a,&b);
+    (a==b) ? printf("equal") : printf("notequal") ;
+}
